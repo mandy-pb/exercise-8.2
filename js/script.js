fdc40d47151e3e5a85068d7fd6a382fe2c7c9e54
Mandy PB <plaisance.mandy@gmail.com>
2023-07-03 23:35:14 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var stuff = [
+  "cars",
+  "blankets",
+  "sunflowers",
+  "sky",
+  "yellow",
+  "code",
+  "perfume",
+  "coffee",
+  "books"
+];
+//stuff.forEach(function (item, index) {
+//if (index % 2 === 0) console.log(item);
+//});
+
+var item = stuff.indexOf("perfume");
+//console.log(item);
+
+//var removeItem = [] {
+// if (item !== 6) {
+//  console.log(`${item} no longer exists.`);
+// } else {
+//  stuff.splice(1,6);
+// }
+//}
+
+var removeItem = function (array, item) {
+  var index = array.indexOf(item);
+
+  if (index === -1) {
+    console.log(`Sorry, no such item in this array.`);
+  } else {
+    array.splice(index, 1);
+    console.log(`Removing ${item}`);
+  }
+};
+
+removeItem(stuff, "perfume");
+console.log(stuff);
+removeItem(stuff, "umbrella");
+console.log(stuff);
